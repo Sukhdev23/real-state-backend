cd6edf2a76f88e4da77e6ae4d6c2e6fdb8013b57
sukhu thakur <sukhethakur128@gmail.com>
2025-03-07 14:55:53 +0530

    8080

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 
 // ✅ Fix: Proper CORS Configuration
 app.use(cors({
-  origin: ["https://peachpuff-oryx-197319.hostingersite.com", "http://localhost:5173"], // Add your frontend domains
+  origin: ["https://peachpuff-oryx-197319.hostingersite.com", "http://localhost:8080"], // Add your frontend domains
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
